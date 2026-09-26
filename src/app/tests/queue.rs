26668f3b5f3e1c1b1eceb60e25978f74cb058dd7
slatkin <slatkin@woims.net>
2026-09-26 10:56:13 +0100

    test: prune app test families to 0 combined cases

--- src/app/tests/queue.rs
+++
@@ -1,5 +0,0 @@
-mod consume;
-mod mutation;
-mod regression;
-mod reorder;
-mod scope;
--- src/app/tests/routing_matrix.rs
+++
@@ -1,7 +0,0 @@
-//! Routing matrix tests are split by logical routing seam.
-
-mod blocking;
-mod focus;
-mod globals;
-mod playback;
-mod support;
